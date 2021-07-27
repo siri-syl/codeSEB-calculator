8c0d4dae8bec7a0e871e86d529d06788231e719f
siri-syl <87490361+siri-syl@users.noreply.github.com>
2021-07-27 14:54:33 +0900

    SEB 33기 이선영 Calculate - Nightmare 과제 제출합니다.

--- script.js
+++ script.js
@@ -22,68 +22,68 @@ function calculate(n1, operator, n2) {
   return String(result);
 }
 
-// let firstNumber = "";
-// let secondNumber = "";
-// let op = "";
+let firstNumber1 = "";
+let secondNumber1 = "";
+let op1 = "";
   
 
-// buttons.addEventListener('click', function (event) {
-//   // 버튼을 눌렀을 때 작동하는 함수입니다.
+buttons.addEventListener('click', function (event) {
+  // 버튼을 눌렀을 때 작동하는 함수입니다.
 
-//   const target = event.target; // 클릭된 HTML 엘리먼트의 정보가 저장되어 있습니다.
-//   const action = target.classList[0]; // 클릭된 HTML 엘리먼트에 클레스 정보를 가져옵니다.
-//   const buttonContent = target.textContent; // 클릭된 HTML 엘리먼트의 텍스트 정보를 가져옵니다.
-//   // ! 위 코드(Line 19 - 21)는 수정하지 마세요.
+  const target = event.target; // 클릭된 HTML 엘리먼트의 정보가 저장되어 있습니다.
+  const action = target.classList[0]; // 클릭된 HTML 엘리먼트에 클레스 정보를 가져옵니다.
+  const buttonContent = target.textContent; // 클릭된 HTML 엘리먼트의 텍스트 정보를 가져옵니다.
+  // ! 위 코드(Line 19 - 21)는 수정하지 마세요.
     
 
-//   if (target.matches('button')) {
-//     // TODO : 계산기가 작동할 수 있도록 아래 코드를 수정하세요. 작성되어 있는 조건문과 console.log를 활용하시면 쉽게 문제를 풀 수 있습니다.
-//     // 클릭된 HTML 엘리먼트가 button이면
-//     if (action === 'number') {
-//       // 그리고 버튼의 클레스가 number이면
-//       // 아래 코드가 작동됩니다.
+  if (target.matches('button')) {
+    // TODO : 계산기가 작동할 수 있도록 아래 코드를 수정하세요. 작성되어 있는 조건문과 console.log를 활용하시면 쉽게 문제를 풀 수 있습니다.
+    // 클릭된 HTML 엘리먼트가 button이면
+    if (action === 'number') {
+      // 그리고 버튼의 클레스가 number이면
+      // 아래 코드가 작동됩니다.
       
-//       console.log('숫자 ' + buttonContent + ' 버튼');
+      console.log('숫자 ' + buttonContent + ' 버튼');
       
-//       if(firstNumber === "" && secondNumber === ""){
-//         firstOperend.textContent = buttonContent;
-//         firstNumber = buttonContent;
-//         console.log(firstNumber);
-//       } else if(firstNumber!== "" && secondNumber === ""){
-//         secondOperend.textContent = buttonContent;
-//         secondNumber = buttonContent
-//         console.log(secondNumber);
-//       }
-//     }
+      if(firstNumber1 === "" && secondNumber1 === ""){
+        firstOperend.textContent = buttonContent;
+        firstNumber1 = buttonContent;
+        console.log(firstNumber1);
+      } else if(firstNumber1!== "" && secondNumber1 === ""){
+        secondOperend.textContent = buttonContent;
+        secondNumber1 = buttonContent
+        console.log(secondNumber1);
+      }
+    }
 
-//     if (action === 'operator') {
-//       console.log('연산자 ' + buttonContent + ' 버튼');
-//       operator.textContent = buttonContent;
-//       op = buttonContent;
-//     }
+    if (action === 'operator') {
+      console.log('연산자 ' + buttonContent + ' 버튼');
+      operator.textContent = buttonContent;
+      op1 = buttonContent;
+    }
 
-//     if (action === 'decimal') {
-//       console.log('소수점 버튼');
-//       // 숫자가 입력되어 있을 때
-//     }
+    if (action === 'decimal') {
+      console.log('소수점 버튼');
+      // 숫자가 입력되어 있을 때
+    }
 
-//     if (action === 'clear') {
-//       console.log('초기화 버튼');
-//       firstOperend.textContent = 0;
-//       secondOperend.textContent = 0;
-//       calculatedResult.textContent = 0;
-//       operator.textContent = '+';
-//       firstNumber = "";
-//       secondNumber = "";
-//       op = "";
-//     }
+    if (action === 'clear') {
+      console.log('초기화 버튼');
+      firstOperend.textContent = 0;
+      secondOperend.textContent = 0;
+      calculatedResult.textContent = 0;
+      operator.textContent = '+';
+      firstNumber1 = "";
+      secondNumber1 = "";
+      op1 = "";
+    }
 
-//     if (action === 'calculate') {
-//       console.log('계산 버튼');
-//       calculatedResult.textContent = calculate(firstNumber, op, secondNumber);
-//     }
-//   }
-// });
+    if (action === 'calculate') {
+      console.log('계산 버튼');
+      calculatedResult.textContent = calculate(firstNumber, op, secondNumber);
+    }
+  }
+});
 
 
 // ! Advanced Challenge test와 Nightmare test를 위해서는 아래 주석을 해제하세요.
@@ -91,11 +91,10 @@ function calculate(n1, operator, n2) {
 const display = document.querySelector('.calculator__display--for-advanced'); // calculator__display 엘리먼트와, 그 자식 엘리먼트의 정보를 모두 담고 있습니다.
 let firstNum, operatorForAdvanced, previousKey, previousNum;
 
-
-let firstNumber = ""; // 첫 번째 숫자
-let secondNumber = ""; // 두 번째 숫자
-let op = ""; // 연산자
-let deci = false;  // 소수점
+let deci = false;  // 소수점let firstNumber1 = "";
+let firstNumber = "";
+let secondNumber = "";
+let op = "";
 
 buttons.addEventListener('click', function (event) {
   // 버튼을 눌렀을 때 작동하는 함수입니다.
@@ -168,11 +167,10 @@ buttons.addEventListener('click', function (event) {
         console.log('소수점 네 번째문');
     }
   }
-  // 1 0 0 . . 1 2 5 2 + 1 2 + 1 5 - - 2 3 - 1 4 4 2
     if (action === 'clear') {
       console.log('초기화 버튼');
       display.textContent = 0;
-      operator.textContent = '';
+      operator.textContent = '+';
       firstNumber = "";
       secondNumber = "";
       op = "";
